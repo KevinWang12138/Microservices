ebc3ab769352eea4b38ba7b7c8cc209184c0209b
KevinWang12138 <wangqianming12138@gmail.com>
2022-06-16 16:36:55 +0800

    Ribbon负载均衡配置，设置饥饿加载

--- cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,7 +2,10 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,15 @@ public class OrderController {
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
+    }
+
+    /**
+     * 配置负载均衡规则，覆盖原有的规则
+     * @return
+     */
+    @Bean
+    public IRule randomRule(){
+        return new RandomRule();
     }
 
 }
